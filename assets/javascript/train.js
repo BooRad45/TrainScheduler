e65025b584f6a885f2b659d54a3925b1e53391b5
Brendan Rogers <brendan@utexas.edu>
2017-03-31 00:01:09 -0500

    got js working.

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -5,42 +5,71 @@ var config = {
     storageBucket: "train-scheduler-d27cd.appspot.com",
     messagingSenderId: "9085562345"
 };
+
 firebase.initializeApp(config);
 
-var name = "";
-var destination = "";
-var start = "";
-var rate = 0;
-var monthsWorked = 0;
-var totalBilled = 0;
-var currentDate = moment().format('MM/DD/YY');
+var database = firebase.database();
+
+// 2. Button for adding Trains
 
 $("#submit").on("click", function(event){
 	event.preventDefault();
-	// adding user input into an array called row
-	var row = [
-		name = $("#trainName").val().trim(),
-		destination = $("#destination").val().trim(),
-		// using moment to grab start date user submitted in order to be able to calculate months worked
-		start = moment($("#firstTrain").val().trim()).format('MM/DD/YY'),
-		//using .diff to subtract start date from current date, we want the result in months, turn result to an integer
-		monthsWorked = parseInt( moment().diff(moment($("#firstTrain").val().trim()), 'months') ),
-		rate = parseInt($("#frequency").val().trim()),
-		totalBilled =  monthsWorked * rate
-	]
-	console.log(start);
-	// creating a new table row when submit is clicked
-	var newRow = $("<tr>");
-	// adding table data for each variable in row array
-	for(i=0;i<6;i++){
-  		var data = $("<td>").text(row[i]);
-  		newRow.append(data);
-	}; 
-	// adding new row to top of table
-	$("#rows").prepend(newRow);	
-	// clearing forms after you submit
-	$(".form-control").val("");
-	console.log("$"+totalBilled);
-	console.log(monthsWorked)
+	
+		trainName = $("#trainName").val().trim();
+		destination = $("#destination").val().trim();
+		// using moment to grab start 
+		firstTrainTime = moment($("#firstTrain").val().trim()).format('HHmm');
+		//using .diff to subtract start fros
+		nextArrival = parseInt( moment().diff(moment($("#firstTrain").val().trim()) ) );
+		frequencyMin = parseInt($("#frequency").val());
+		
 
-})
+var newTrainInfo = {
+    name: trainName,
+    dest: destination,
+    firstTrain: firstTrainTime,
+    frequency: frequencyMin
+  };
+
+console.log(firstTrainTime);
+
+  // Uploads train data to the database
+  database.ref().push(newTrainInfo);
+
+  // Logs everything to console
+  console.log(newTrainInfo.name);
+  console.log(newTrainInfo.dest);
+  console.log(newTrainInfo.firstTrain);
+  console.log(newTrainInfo.frequency);
+
+  // Alert
+  alert("Train successfully added");
+
+  $(".form-control").val("");
+
+});
+
+// 3. Create Firebase event for adding train to the database and a row in the html when admin adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().name;
+  var destination = childSnapshot.val().dest;
+  var firstTrainTime = childSnapshot.val().firsTrain;
+  var frequencyMin = childSnapshot.val().frequency;
+
+  // Train Info
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequencyMin);
+
+
+	// Add each train's data into the table
+  $("#current-trains > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" +
+  firstTrainTime);
+
+});
+
